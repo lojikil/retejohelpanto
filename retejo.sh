3ced90f1d1c84905fb0fcfb081c1304f25e51065
Stefan Edwards <saedwards.ecc@gmail.com>
2020-11-01 20:02:49 -0500

    minor bug fixes to sidebar generation

--- retejo.sh
+++ retejo.sh
@@ -8,9 +8,11 @@ cat header.html
 # generate the side bar
 echo '  <div id="side-bar">'
 echo '    <div>'
+echo '      <ul>'
 
 for i in `find . -type d -depth 1`
 do
+    i=`echo $i | sed -e 's/\.\///'`
     if [ "$i" = "./.git" ]; then
         continue
     elif [ "$i" = "$base" ]; then
@@ -18,20 +20,22 @@ do
         # TODO (lojikil) currently only matches on depth
         # we need to check prefixes...
 
-        echo '    <li> <a href="'$i'">&rsaquo; ' $i '</a></li>'
+        echo '    <li> <a href="/'$i'">&rsaquo; ' $i '</a></li>'
         echo '    <li>'
         echo '      <ul>'
         for d in `find $i -type d -depth 1`
         do
-            echo '         <li><a href="'$d'">&rsaquo;' $d '</a></li>'
+            d=`echo $d | sed -e 's/\.\///'`
+            echo '         <li><a href="/'$d'">&rsaquo;' $d '</a></li>'
         done
         echo '      </ul>'
         echo '    </li>'
     else
-        echo '    <li> <a href="'$i'">&rsaquo;' $i '</a></li>'
+        echo '    <li> <a href="/'$i'">&rsaquo;' $i '</a></li>'
     fi
 done
 
+echo '      </ul>'
 echo '    </div>'
 echo '  </div>'
 echo 
