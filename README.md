15f8257bc76b0611039f75527fbf6706e1f371a0
Stefan Edwards <saedwards.ecc@gmail.com>
2020-11-01 19:52:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Overview
+
+A simple [werc-like](http://werc.cat-v.org/) script for generating static websites thusly: `sh ./retejo.sh ./p > ./p/index.html`
+
+# Superrigardo
+
+Simpla [werc-simila](http://werc.cat-v.org/) skripto por generatu statikojn retejojn tiel: `sh ./retejo.sh ./p > ./p/index.html`
---
+++ retejo.sh
@@ -0,0 +1,41 @@
+set -e
+base=$1
+
+# generate the header
+
+cat header.html
+
+# generate the side bar
+echo '  <div id="side-bar">'
+echo '    <div>'
+
+for i in `find . -type d -depth 1`
+do
+    if [ "$i" = "./.git" ]; then
+        continue
+    elif [ "$i" = "$base" ]; then
+        echo '    <li> <a href="'$i'">&rsaquo; ' $i '</a></li>'
+        echo '    <li>'
+        echo '      <ul>'
+        for d in `find $i -type d -depth 1`
+        do
+            echo '         <li><a href="'$d'">&rsaquo;' $d '</a></li>'
+        done
+        echo '      </ul>'
+        echo '    </li>'
+    else
+        echo '    <li> <a href="'$i'">&rsaquo;' $i '</a></li>'
+    fi
+done
+
+echo '    </div>'
+echo '  </div>'
+echo 
+echo
+echo '  <div id="main-copy">'
+
+pandoc -f markdown -t html $base/index.txt
+
+echo '  </div>'
+
+cat footer.html
